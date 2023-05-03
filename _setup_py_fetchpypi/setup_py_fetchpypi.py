a83e42dd42cf578c40701e3e89dfd79812cd2b76
rydnr <github@acm-sl.org>
2023-05-03 12:33:42 +0200

    Splitted setup.py recipe into fetchPypi and fetchFromGitHub

---
+++ _setup_py_fetchpypi/setup_py_fetchpypi.py
@@ -0,0 +1,19 @@
+from domain.flake import Flake
+from domain.recipe.base_flake_recipe import BaseFlakeRecipe
+
+class SetuppyFetchPypi(BaseFlakeRecipe):
+
+    """
+    Represents a nix flake recipe for setup.py-based projects using fetchPypi.
+    """
+    def __init__(self, flake: Flake):
+        """Creates a new setup.py+fetchPypi flake recipe instance"""
+        super().__init__(id)
+        self._flake = flake
+
+    def usesPipSha256(self):
+        return True
+
+    @classmethod
+    def type_matches(cls, flake) -> bool:
+        return flake.python_package.get_type() == "setup.py" and flake.python_package.package_in_pypi()
